317da443a7455dc9142779e1a7d8b76b99bcc0ff
Jonas Birmé <jonas.birme@eyevinn.se>
2025-02-12 17:54:48 +0100

    fix typo

--- osc/docker-entrypoint.sh
+++ osc/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 export BASE_URL="https://${OSC_HOSTNAME}"
-export SECRET_KEY_BASE=$(openssl rand -base64 48)"
+export SECRET_KEY_BASE="$(openssl rand -base64 48)"
 export HTTP_PORT=${PORT:-8080}
 
 if [ "$1" = 'run' ]; then
